cd6ae0afc93f6cb979e2e7139e13b6f1d21861c0
Nguyễn Trương Đăng Quang <quangnguyentruongdang@gmail.com>
2026-06-16 09:08:01 +0700

    Nothing

--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -10,55 +10,29 @@ return {
                     completion = { enable = false },
                     inline_completion = { enable = false }
                 },
-                -- virtualtext = {
-                --     auto_trigger_ft = {},
-                --     keymap = {
-                --         -- accept whole completion
-                --         accept = '<A-A>',
-                --         -- accept one line
-                --         accept_line = '<A-a>',
-                --         -- accept n lines (prompts for number)
-                --         -- e.g. "A-z 2 CR" will accept 2 lines
-                --         accept_n_lines = '<A-z>',
-                --         -- Cycle to prev completion item, or manually invoke completion
-                --         prev = '<A-[>',
-                --         -- Cycle to next completion item, or manually invoke completion
-                --         next = '<A-]>',
-                --         dismiss = '<A-e>',
-                --     },
-                -- },
-                provider = 'openai_fim_compatible',
-                n_completions = 1, -- recommend for local model for resource saving
-                -- I recommend beginning with a small context window size and incrementally
-                -- expanding it, depending on your local computing power. A context window
-                -- of 512, serves as an good starting point to estimate your computing
-                -- power. Once you have a reliable estimate of your local computing power,
-                -- you should adjust the context window to a larger value.
+                virtualtext = {
+                    auto_trigger_ft = { '*' },
+                    keymap = {
+                        accept = '<A-A>',
+                        accept_line = '<A-a>',
+                        accept_n_lines = '<A-z>',
+                        prev = '<A-[>',
+                        next = '<A-]>',
+                        dismiss = '<A-e>',
+                    },
+                },
+                provider = 'openai_compatible',
+                n_completions = 1,
                 context_window = 1024,
                 provider_options = {
-                    openai_fim_compatible = {
-                        -- For Windows users, TERM may not be present in environment variables.
-                        -- Consider using APPDATA instead.
+                    openai_compatible = {
                         api_key = 'TERM',
                         name = 'Ollama',
-                        end_point = 'http://localhost:11434/v1/completions',
-                        model = 'qwen3-coder:480b-cloud',
+                        end_point = 'http://localhost:11434/v1/chat/completions',
+                        model = 'qwen3-coder-next:cloud',
                         optional = {
                             max_tokens = 70,
                             top_p = 0.9,
-                        },
-                        -- Llama.cpp does not support the `suffix` option in FIM completion.
-                        -- Therefore, we must disable it and manually populate the special
-                        -- tokens required for FIM completion.
-                        template = {
-                            prompt = function(context_before_cursor, context_after_cursor, _)
-                                return '<|fim_prefix|>'
-                                    .. context_before_cursor
-                                    .. '<|fim_suffix|>'
-                                    .. context_after_cursor
-                                    .. '<|fim_middle|>'
-                            end,
-                            suffix = false,
                         },
                     },
                 },
