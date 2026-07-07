df48a23f0d70631f6e73343062462179854de236
quangnguyen0101 <quangdangtruongnguyen@gmail.com>
2026-07-07 21:36:58 +0700

    Sync improvements from nixos-config

--- nvim-config/lua/plugins/comment-nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'numToStr/Comment.nvim',
-    opts = {
-        -- add any options here
-    }
-}
-
--- comment configs in init.lua
--- nvim-config/lua/plugins/lualine.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
-}
-
--- lualine config in init.lua
